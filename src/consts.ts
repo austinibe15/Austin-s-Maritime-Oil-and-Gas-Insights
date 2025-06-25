acfd36e0fd2672def805da94eb518fb517738161
Austin Ibe <austinibe15@gmail.com>
2025-06-25 14:47:55 -0700

    Update build process info and fix import issues

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
+
 // src/consts.js
 export const SITE_TITLE = "Austin’s Maritime & Oil & Gas Insights";
 export const SITE_TAGLINE = "Navigating Nigeria’s maritime and oil sectors—diplomacy, logistics, and investment.";
+export const SITE_DESCRIPTION = "Your resource for Nigeria's maritime and oil & gas insights.";
